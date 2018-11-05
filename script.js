3fd6c51c3d035b6a74529f91b788f4ce6ae7c570
Christian Sparks <csparks1919@hotmail.com>
2018-11-05 11:18:05 -0600

    Cleaned and added dat.gui

--- script.js
+++ script.js
@@ -1,78 +1,180 @@
-let scene, clock, camera, renderer, cube;
+/* Sorry if you're reading this, the code is a mess, but it does cool stuff */
+
+let controlToggle = true;
+
+document.addEventListener("keydown", function (event) {
+	if (event.keyCode === 27) controlToggle = !controlToggle
+});
+
+let scene = new THREE.Scene();
+let clock, camera, renderer, cube;
 let animIndex;
 
-let topTex = new THREE.TextureLoader().load('Top.png')
-let side1Tex = new THREE.TextureLoader().load('Side1.png')
-let side2Tex = new THREE.TextureLoader().load('Side2.png')
-let side3Tex = new THREE.TextureLoader().load('Side3.png')
-let side4Tex = new THREE.TextureLoader().load('Side4.png')
-let botTex = new THREE.TextureLoader().load('Bottom.png')
+
+let Planet = {
+	seed: Math.round(Math.random()*65535),
+	hasWater: true,
+	wireframe: false,
+	waterScale: 0.7,
+	res: 100,
+	waterColor: 0x8888ff,
+	addColor: addColor,
+	colorMapLength: 5,
+	regenGeometry: genGeometry,
+	scale: 2
+}
+
+let objectList = [];
+
+let gui = new dat.GUI()
+let scale = gui.add(Planet, 'scale', 0, 10)
+let seed = gui.add(Planet, 'seed', 0,65535)
+let waterCtl = gui.add(Planet, 'hasWater')
+let wfCtl = gui.add(Planet, 'wireframe')
+let sclCtl = gui.add(Planet, 'waterScale', -2,2)
+let resCtl = gui.add(Planet, 'res', 25, 300)
+let wclrCtl = gui.addColor(Planet, 'waterColor')
+let colorCtl = gui.addFolder("Colors")
+gui.add(Planet, "regenGeometry")
+
+
+initColors()
+console.log(Planet)
+function initColors() {
+	let colorArray = [[ 0.0, 0xffffff ], [ 0.2, 0xdddddd ], [ 0.5, 0xdddd00 ], [ 0.75, 0x44FF44 ], [ 1.0, 0xdddddd ]]
+	
+	for(color in colorArray) {
+		//this is a hack because dat.gui doesn't support arrays in objects
+		Planet["color"+color] = colorArray[color][1]
+		Planet["offset"+color] = colorArray[color][0]
+		colorCtl.addColor(Planet, "color"+color)
+		colorCtl.add(Planet, "offset"+color, 0, 1)
+	}
+}
+
+function colorsToColorMap() {
+	//hack because javascript
+	let colorMap = []
+	console.log(JSON.parse(JSON.stringify(Planet)))
+	for(let i = 0; i < Planet.colorMapLength; i++) {
+		console.log("color"+i)
+		let str = "color"+i
+		let stro = "offset"+i
+		let clr = Planet[str]
+		let offset = Planet[stro]
+		colorMap.push([offset,clr])
+	}
+	console.log(colorMap)
+	return colorMap;
+}
+
+function addColor() {
+	Planet.colorMapLength++;
+	Planet["color"+color] = 0xFFFFFF;
+	Planet["color"+color+" offset"] = 1.0;
+	colorCtl.addColor(Planet, "color"+color)
+	colorCtl.add(Planet, "offset"+color, 0, 1)
+
+}
+
 
 init();
 animate();
+
+
 function init() {
-	scene = new THREE.Scene();
 	clock = new THREE.Clock();
 
 	camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 10000 );
+	camera.position.x = -208
+	camera.position.y = 16
+	camera.position.z = -46
 	controls = new THREE.FirstPersonControls( camera );
 	controls.movementSpeed = 100;
 	controls.lookSpeed = .3;
 	controls.lookVertical = true;
-
-	let geometry = new THREE.BoxBufferGeometry( 1, 1, 1, 100, 100, 100);
-	console.log(geometry)
-	geometry = normalizeSphere(geometry);
-
-	geometry = generateTerr(geometry);
-	geometry = generateSimplexVertexColors(geometry);
-	geometry.computeVertexNormals();
-	let material = new THREE.MeshPhongMaterial( {vertexColors: THREE.VertexColors} );
-	/*let material2 = new THREE.MeshBasicMaterial( {color: 0xffffff} );
-	let material = [material1, material2,material2,material2,material2,material2]*/
-	//let material = generateEarthCubemap();
-	cube = new THREE.Mesh( geometry, material );
-	cube.scale.set(100,100,100)
-	scene.add( cube );
-
-	let waterGeo = new THREE.SphereBufferGeometry(1, 24, 24)
-	let waterMat = new THREE.MeshPhongMaterial({color: 0x0000ff})
-	let water = new THREE.Mesh( waterGeo, waterMat);
-	water.scale.set(100,100,100)
-	scene.add(water)
-
-	var wireframe = new THREE.WireframeGeometry( geometry );
-
-	var line = new THREE.LineSegments( wireframe );
-	line.material.depthTest = false;
-	line.material.opacity = 0.25;
-	line.material.transparent = true;
-	line.scale.set(100,100,100)
-	//scene.add( line );
+	
+        controls.object.rotation.x = -2
+        controls.object.rotation.y = .95
+        controls.object.rotation.z = 2.15
+	
+	genGeometry()
 
 	var lights = [];
-		lights[ 0 ] = new THREE.PointLight( 0x888888, 1, 0 );
-		lights[ 1 ] = new THREE.PointLight( 0x888888, 1, 0 );
-		lights[ 2 ] = new THREE.PointLight( 0x888888, 1, 0 );
-		lights[ 0 ].position.set( 200, 200, 0 );
-		lights[ 1 ].position.set( 110, 200, 110 );
-		lights[ 2 ].position.set( - 110, - 200, - 110 );
+		lights[ 0 ] = new THREE.PointLight( 0xbbbbbb, 1, 0 );
+		lights[ 1 ] = new THREE.PointLight( 0xbbbbbb, 1, 0 );
+		lights[ 0 ].position.set( -200, 200, 0 );
+		lights[ 1 ].position.set( -110, 200, 110 );
 		scene.add( lights[ 0 ] );
 		scene.add( lights[ 1 ] );
-		scene.add( lights[ 2 ] );/*
-	let light = new THREE.DirectionalLight(0xffffff, 1)
-	light.rotation.set(0,90,0)
+	/*let light = new THREE.DirectionalLight(0xffffff, 1)
+	light.rotation.set(30,40,30)
 	scene.add(light)*/
 
 
-	renderer = new THREE.WebGLRenderer();
+	renderer = new THREE.WebGLRenderer({antialias: true});
 	//050510
-	renderer.setClearColor(0xffffff, 1);
+	renderer.setClearColor(0x111133, 1);
 	renderer.setSize( window.innerWidth, window.innerHeight );
 	document.body.appendChild( renderer.domElement );
 	window.addEventListener( 'resize', onWindowResize, false );
+	controls.update( clock.getDelta() )
+}
+
+function genGeometry() {
+	console.log(Planet);
+	console.log(objectList)
+	for(let obj in objectList) {
+		let selectedObj = scene.getObjectByName(obj.name)
+		console.log(obj)
+		console.log(selectedObj)
+		
+		scene.remove(objectList[obj])
+
+	}
+	
+	objectList = [];
+
+	let geometry = new THREE.BoxBufferGeometry( 1, 1, 1, Planet.res, Planet.res, Planet.res);
+        console.log(geometry)
+        geometry = normalizeSphere(geometry);
+	
+	geometry = generateTerr(geometry);
+        geometry = generateSimplexVertexColors(geometry);
+	
+	geometry.computeVertexNormals();
+        let material = new THREE.MeshPhongMaterial( {vertexColors: THREE.VertexColors} );
+	
+	cube = new THREE.Mesh( geometry, material );
+        cube.scale.set(100,100,100)
+        scene.add( cube );
+	objectList.push( cube );
+	
+	console.log("Planet has water"+Planet.hasWater)
+	if(Planet.hasWater === true) {
+		console.log("asdf")
+		let waterGeo = new THREE.SphereBufferGeometry(1, 24, 24)
+        	let waterMat = new THREE.MeshPhongMaterial({color: Planet.waterColor})
+        	let water = new THREE.Mesh( waterGeo, waterMat);
+        	water.scale.set(Planet.waterScale+100, Planet.waterScale+100, Planet.waterScale+100)
+        	scene.add(water)
+		objectList.push(water);
+	}
+
+	if (Planet.wireframe) {
+		var wireframe = new THREE.WireframeGeometry( geometry );
+
+        	var line = new THREE.LineSegments( wireframe );
+        	line.material.depthTest = false;
+        	line.material.opacity = 0.75;
+        	line.material.transparent = true;
+        	line.scale.set(100,100,100)
+        	scene.add( line );
+		objectList.push(line)
+	}
 
 }
+
 
 function onWindowResize(){
 
@@ -86,7 +188,7 @@ function onWindowResize(){
 function animate() {
 	requestAnimationFrame( animate );
 	renderer.render( scene, camera );
-	controls.update( clock.getDelta() );
+	if(controlToggle) {controls.update( clock.getDelta() )};
 }
 
 function generateVertexColors(vertexGeometry) {
@@ -98,7 +200,6 @@ function generateVertexColors(vertexGeometry) {
 		colorsArr.push(cols[1]/255);
 		colorsArr.push(cols[2]/255);
 	}
-	console.log(colorsArr);
 	let colors = new Float32Array(colorsArr);
 
 	// itemSize = 3 because there are 3 values (components) per vertex
@@ -110,14 +211,13 @@ function generateVertexColors(vertexGeometry) {
 function generateTerr(sphereGeometry) {
 	sphereGeometry.computeVertexNormals()
 	for(let i = 0; i < sphereGeometry.attributes.position.array.length; i=i+3) {
-		let nout = noiseGen(sphereGeometry.attributes.position.array[i],sphereGeometry.attributes.position.array[i+1],sphereGeometry.attributes.position.array[i+2], 4, 6, .75);
-		sphereGeometry.attributes.position.array[i] += sphereGeometry.attributes.normal.array[i]*(nout/100);
-		sphereGeometry.attributes.position.array[i+1] += sphereGeometry.attributes.normal.array[i+1]*(nout/100);
-		sphereGeometry.attributes.position.array[i+2] += sphereGeometry.attributes.normal.array[i+2]*(nout/100);
+		let nout = noiseGen(sphereGeometry.attributes.position.array[i],sphereGeometry.attributes.position.array[i+1],sphereGeometry.attributes.position.array[i+2], 2.5, 6, .75);
+		sphereGeometry.attributes.position.array[i] += sphereGeometry.attributes.normal.array[i]*((nout/100)*Planet.scale);
+		sphereGeometry.attributes.position.array[i+1] += sphereGeometry.attributes.normal.array[i+1]*((nout/100)*Planet.scale);
+		sphereGeometry.attributes.position.array[i+2] += sphereGeometry.attributes.normal.array[i+2]*((nout/100)*Planet.scale);
 		//console.log(sphereGeometry.attributes.position.array[i+2])
 		//console.log(sphereGeometry.attributes.normal.array[i+2])
 	}
-	console.log(sphereGeometry)
 	return sphereGeometry
 }
 
@@ -133,7 +233,6 @@ function normalizeSphere(sphereGeometry) {
 		sphereGeometry.attributes.position.array[i+1] = sy
 		sphereGeometry.attributes.position.array[i+2] = sz
 	}
-	console.log(sphereGeometry)
 	return sphereGeometry;
 }
 
@@ -174,27 +273,57 @@ function generateEarthCubemap() {
 
 function generateSimplexVertexColors(vertexGeometry) {
 	let colorsArr = [];
+	let colorMap = colorsToColorMap();
+	THREE.ColorMapKeywords["terrainMap"] = colorMap
+	let lut = new THREE.Lut("terrainMap", 512)
+	/*console.log(colorMap)
+	lut.addColorMap( "terrainMap", colorMap )
+	lut = lut.changeColorMap("terrainMap");
+	*/lut.setMin(0.05)
+	lut.setMax(0.95)
+	console.log(lut)
 	for(let i = 0; i < vertexGeometry.attributes.position.array.length; i=i+3) {
 		let ox = vertexGeometry.attributes.position.array[i];
 		let oy = vertexGeometry.attributes.position.array[i+1];
 		let oz = vertexGeometry.attributes.position.array[i+2];
-		let nout = noiseGen(ox,oy,oz, 4, 6, .75); //noise.simplex3(ox,oy,oz)
+		let nout = noiseGen(ox,oy,oz, 2.5, 6, .75); //noise.simplex3(ox,oy,oz)
 		nout = (nout+2)/4
-		if(nout <0.5) {
+		
+		let color = lut.getColor(nout);
+		colorsArr.push(color.r)
+		colorsArr.push(color.g)
+		colorsArr.push(color.b)
+		/*if(nout <0.57) {
+			/*colorsArr.push(202/255);
+                        colorsArr.push(100/255);
+                        colorsArr.push(242/255);
+			colorsArr.push(nout)
+			colorsArr.push(nout)
+			colorsArr.push(nout)
+			
 			colorsArr.push(255/255);
 			colorsArr.push(210/255);
 			colorsArr.push(5/255);
 		}
 		else if(nout < 0.75) {
+			/*colorsArr.push(202/255);
+                        colorsArr.push(80/255);
+                        colorsArr.push(242/255)*/
+			/*colorsArr.push(nout)
+			colorsArr.push(nout)
+			colorsArr.push(nout)
 			colorsArr.push(146/255);
 			colorsArr.push(255/255);
 			colorsArr.push(36/255);
 		}
 		else {
+			/*colorsArr.push(162/255);
+                        colorsArr.push(60/255);
+                        colorsArr.push(242/255)
 			colorsArr.push(nout);
 			colorsArr.push(nout);
 			colorsArr.push(nout);
-		}
+		}*/
 		/*let cols = HSVtoRGB(nout, 1, 1)
 		colorsArr.push(cols[0]/255);
 		colorsArr.push(cols[1]/255);
@@ -203,7 +332,6 @@ function generateSimplexVertexColors(vertexGeometry) {
 		colorsArr.push(nout);
 		colorsArr.push(nout);*/
 	}
-	console.log(colorsArr);
 	let colors = new Float32Array(colorsArr);
 
 	// itemSize = 3 because there are 3 values (components) per vertex
@@ -215,6 +343,7 @@ function generateSimplexVertexColors(vertexGeometry) {
 function noiseGen(ix,iy,iz,scale,octaves,persistence) {
 	//let heightmap = [...Array(w)].map(i => Array(l));
 	let nout = 0;
+	noise.seed(Planet.seed)
 	/*for(let i = 0; i < octaves; i++){
 		seeds.push(Math.random());
 	}*/
